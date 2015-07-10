2bb00631483b1f0b591579b6a5bf4abdec8193dc
Rob Penty <robpenty@gmail.com>
2015-07-10 16:55:07 -0400

    Committing after Day 22 Lesson, updating Customers

--- public/modules/customers/controllers/customers.client.controller.js
+++ public/modules/customers/controllers/customers.client.controller.js
@@ -19,6 +19,16 @@ customersApp.controller('CustomersController', ['$scope', '$stateParams', '$loca
 				templateUrl: 'modules/customers/views/edit-customer.client.view.html',
 				controller: function ($scope, $modalInstance, customer) {
 					$scope.customer = customer;
+
+
+					$scope.ok = function () {
+					    $modalInstance.close($scope.customer);
+					};
+
+					$scope.cancel = function () {
+					    $modalInstance.dismiss('cancel');
+					};
+
 				},
 				size: size,
 				resolve: {
@@ -43,8 +53,20 @@ customersApp.controller('CustomersCreateController', ['$scope', 'Customers',
 	}
 ]);
 
-customersApp.controller('CustomersEditController', ['$scope', 'Customers',
+customersApp.controller('CustomersUpdateController', ['$scope', 'Customers',
 	function($scope, Customers) {
+
+		// Update existing Customer
+		this.update = function(updatedCustomer) {
+			var customer = updatedCustomer;
+
+			customer.$update(function() {
+				
+			}, function(errorResponse) {
+				$scope.error = errorResponse.data.message;
+			});
+		};
+
 
 	}
 ]);
@@ -104,16 +126,7 @@ customersApp.controller('CustomersEditController', ['$scope', 'Customers',
 			}
 		};
 
-		// Update existing Customer
-		$scope.update = function() {
-			var customer = $scope.customer;
-
-			customer.$update(function() {
-				$location.path('customers/' + customer._id);
-			}, function(errorResponse) {
-				$scope.error = errorResponse.data.message;
-			});
-		};
+		
 
 		
 
